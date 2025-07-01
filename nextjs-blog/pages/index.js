73037fdff4ea4b502cc3f08357f90e53455dcecf
Pawel Wujczyk <pwujczyk@gmail.com>
2025-07-01 21:09:37 +0200

    render md

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,9 +1,10 @@
-import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
 
-import { getSortedPostsData } from '../lib/posts';
- 
+import { getSortedPostsData } from "../lib/posts";
+
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
   return {
@@ -12,23 +13,23 @@ export async function getStaticProps() {
     },
   };
 }
- 
+
 export default function Home({ allPostsData }) {
   return (
     <Layout home>
       {/* Keep the existing code here */}
- 
+
       {/* Add this <section> tag below the existing <section> tag */}
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>{title}</Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
