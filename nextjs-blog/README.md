887594dd790d8fa9518d3e5ce674d5f8e823b1c1
Pawel Wujczyk <pwujczyk@gmail.com>
2025-07-01 18:15:54 +0200

    doing ablog

---
+++ nextjs-blog/README.md
@@ -0,0 +1,7 @@
+[Tutorial](https://nextjs.org/learn/pages-router/create-nextjs-app-setup)
+
+```
+npm run dev
+```
+
+http://localhost:3000/
---
+++ nextjs-blog/pages/_app.js
@@ -0,0 +1,6 @@
+import '../styles/global.css';
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+//thisfile is used to use the global.css file
